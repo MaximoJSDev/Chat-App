f7cc9c1442d64792680fc395dca90bcee0232d23
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-29 22:29:25 -0600

    Inicializa el proyecto

---
+++ src/components/VistaAcceso.vue
@@ -0,0 +1,7 @@
+<template>
+    <h5>Formulario de Registro</h5>
+    <form>
+        <label for="email">Email: </label>
+        <input type="email">
+    </form>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <vista-acceso />
+  </div>
+</template>
+
+<script>
+import VistaAcceso from '../components/VistaAcceso.vue'
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    VistaAcceso
+  }
+}
+</script>
