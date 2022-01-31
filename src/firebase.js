dbb409b18d31aa37bb669a6426e915dcce9d1972
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-31 17:58:06 -0600

    Configura el metodo getAuth de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,8 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const db = app.firestore()
-const auth = app.auth()
-const marcaTiempo = app.firestore.FieldValue.serverTimestamp
-
-export { db, auth, marcaTiempo }
+export const auth = getAuth(app)
