186ab708d63d4d42cfb181e884f5c623e1f075d4
Maximo-4ever <maximilianocm06@gmail.com>
2022-01-31 18:13:35 -0600

    Agrega la ruta: register, login, chat

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Login.vue')
+  },
+  {
+    path: '/chat',
+    name: 'Chat',
+    component: () => import(/* webpackChunkName: "home" */ '../views/Chat.vue')
   }
 ]
 
