b87e8b60859cb189f9cd11eb15637e6f57815002
Maximo-4ever <maximilianocm06@gmail.com>
2022-02-02 15:20:22 -0600

    Agrega la hora del mensaje y le da estilo CSS

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,12 +1,18 @@
 <template>
-    <div
-      v-for="user in users"
-      :key="user.uid"
-      :class="user.state ? 'connected-user' : 'offline-user'"
-    >
-      <h4>{{user.username || user.email}}</h4>
-      <p>Why not? Say hello!</p>
+  <div
+    v-for="user in users"
+    :key="user.uid"
+    :class="user.state ? 'connected-user user' : 'offline-user user'"
+  >
+  <div class="btn-nav profile">
+    <img src="../assets/Profile.svg" alt="Image user">
+  </div>
+    <div>
+      <h6 class="user__title">{{user.username || user.email}}</h6>
+      <p class="user__msg">Why not? Say hello!</p>
     </div>
+      <span class="user__time">09:00</span>
+  </div>
 </template>
 
 <script>
@@ -48,5 +54,27 @@ export default {
 <style>
 .offline-user {
   color: #b0bacc;
+}
+.user {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 10px 0;
+  border-bottom: 1px solid #e1e1e1c7;
+}
+body.dark-mode .user {
+  border-bottom-color: #e5e5e51f;
+}
+.user .profile img {
+  box-shadow: none;
+  margin-right: 14px;
+  background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
+}
+.user__title {
+  font-size: 18px;
+}
+.user__time {
+  text-align: end;
+  flex-grow: 2;
 }
 </style>
