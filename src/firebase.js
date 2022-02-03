2a0595c211f66e4d3daeef2471df2b978a7eaf06
Maximo-4ever <maximilianocm06@gmail.com>
2022-02-03 15:03:45 -0600

    Agrega y exporta el metodo serverTimestamp

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore, serverTimestamp } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const db = getFirestore()
+const timeStamp = serverTimestamp
 
-export { auth, db }
+export { auth, db, timeStamp }
